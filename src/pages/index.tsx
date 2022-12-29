6b62cc85f84cb04439bfbd58f80ed9b7aefd059f
aeither <36173828+aeither@users.noreply.github.com>
2022-12-29 12:30:46 +0100

    add blockie and wallet ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,41 @@ import AccountPicker from '@/components/AccountPicker'
 import PageHeader from '@/components/PageHeader'
 import { EIP155_MAINNET_CHAINS, EIP155_TEST_CHAINS } from '@/data/EIP155Data'
 import SettingsStore from '@/store/SettingsStore'
-import { Text } from '@nextui-org/react'
 import { Fragment } from 'react'
 import { useSnapshot } from 'valtio'
+import { Grid, Card, Text, Avatar, Row, Col } from '@nextui-org/react'
+import { HiDownload } from 'react-icons/hi'
+import { FiExternalLink } from 'react-icons/fi'
+import { AiOutlineSwap } from 'react-icons/ai'
 
 export default function HomePage() {
-  const { testNets, eip155Address } = useSnapshot(SettingsStore.state)
+  const { testNets, eip155Address, account } = useSnapshot(SettingsStore.state)
 
   return (
     <Fragment>
       <PageHeader title="title">
         <AccountPicker />
       </PageHeader>
-      <Text h4 css={{ marginBottom: '$5' }}>
-        Mainnets
-      </Text>
 
-      {/* {EIP155_MAINNET_CHAINS['eip155:50'].logo} */}
+      <Row justify="center" align="center" css={{ padding: '$8' }}>
+        <Col
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '$4'
+          }}>
+          <Avatar
+            css={{ size: '$20' }}
+            src={`/blockie${account}.png`}
+            color="gradient"
+            bordered
+          />
+          <Text h4 css={{ marginBottom: '$5', textAlign: 'center' }}>
+            $221.43
+          </Text>
+        </Col>
+      </Row>
 
       {Object.values(EIP155_MAINNET_CHAINS).map(({ name, logo, rgb }) => (
         <AccountCard
@@ -30,6 +48,54 @@ export default function HomePage() {
           address={eip155Address}
         />
       ))}
+
+      <Grid.Container gap={2} justify="center">
+        <Grid xs={4}>
+          <Card css={{ h: '$24', $$cardColor: '$colors$primary' }}>
+            <Card.Body
+              css={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+              }}>
+              <HiDownload size={40} />
+              <Text h6 size={15} color="white" css={{ mt: 0 }}>
+                Buy
+              </Text>
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={4}>
+          <Card css={{ h: '$24', $$cardColor: '$colors$primary' }}>
+            <Card.Body
+              css={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+              }}>
+              <FiExternalLink size={40} />
+              <Text h6 size={15} color="white" css={{ mt: 0 }}>
+                Send
+              </Text>
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={4}>
+          <Card css={{ h: '$24', $$cardColor: '$colors$primary' }}>
+            <Card.Body
+              css={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+              }}>
+              <AiOutlineSwap size={40} />
+              <Text h6 size={15} color="white" css={{ mt: 0 }}>
+                Swap
+              </Text>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
 
       {testNets ? (
         <Fragment>
